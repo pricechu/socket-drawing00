71a3c777eead4684b6a6bccfc4a7870ab6c81154
Chu Yiu Wai <pricechu@gmail.com>
2025-08-06 19:45:50 +0100

    first commit

---
+++ public/sketch.js
@@ -0,0 +1,33 @@
+var socket;
+
+function setup() {
+  createCanvas(600, 400);
+  background(220);
+  socket = io.connect('http://localhost:3000');
+  socket.on('mouse', newDrawing);
+}
+
+function newDrawing(data) {
+  noStroke();
+  fill(255, 0, 100);
+  ellipse(data.x, data.y, 20, 20);
+}
+
+function mouseDragged() {
+  console.log(mouseX + ',' + mouseY);
+
+var data ={
+  x: mouseX,
+  y: mouseY
+}
+
+socket.emit('mouse', data);
+
+  noStroke();
+  fill(255);
+  ellipse(mouseX, mouseY, 20, 20);
+}
+
+function draw() {
+
+}
